78feb32c6e8e3c5ded32f952beb360ec3ee67190
airforceoneofficiel <elemoullec.e05@gmail.com>
2022-11-15 12:12:26 +0100

    Initial commit

---
+++ mecanique-site.js
@@ -0,0 +1,18 @@
+/* <input type="button" value="Don't show this again! " onclick="fbLikeDump();WriteCookie();" /> */
+
+const lebtn = document.querySelector('.js-lebtn');    
+lebtn.addEventListener('click', ouvrirefenetres);
+
+function ouvrirefenetres(){     
+    
+    const link1 = 'https://cheerful-axolotl-1cd535.netlify.app';     
+    const link2 = 'https://fanciful-halva-f4808e.netlify.app';    
+
+    window.open(link1);     
+    window.open(link2); 
+
+//     setInterval(() => {       
+//         window.open(link1, link2);
+// }, 500);   
+    
+}    
